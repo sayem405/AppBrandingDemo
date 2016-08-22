8a7d97845651804a90957867c214b3c0113f9bb6
M.A. Sayem <sayem.cse18@gmail.com>
2016-08-22 13:12:08 +0600

    init commit
    init commit

---
+++ app/src/main/java/com/sayem/android/appbrandingdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sayem.android.appbrandingdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
